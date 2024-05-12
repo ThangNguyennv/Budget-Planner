7222b72d9a0f17e69de262f9674d3f8a7b7459d3
ThangNguyennv <thangnv1029@gmail.com>
2024-05-12 23:23:18 +0700

    Add login-component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  // Cung cấp thư viện
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Xác định đường dẫn và tải modul về.
+    { path: 'budget-planner', loadChildren:() => import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule)}
+];

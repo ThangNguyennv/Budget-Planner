0c6e96c4b5bd3006901f7ea6456b39d4404f2611
ThangNguyennv <thangnv1029@gmail.com>
2024-05-13 11:28:44 +0700

    Add dashboard component fixed

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { IncomeComponent } from './income/income.component';
 
 const routes: Routes = [
   { path: 'login', component:LoginComponent},
   { path: 'side-nav', component:SideNavComponent},
-  { path: 'dashboard', component:DashboardComponent}
+  { path: 'dashboard', component:DashboardComponent},
+  { path: 'income', component:IncomeComponent},
 ];
 
 @NgModule({
